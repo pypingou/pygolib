e5d9869f4830bd8f2e7b946aca19b6110ae7d800
Pierre-Yves Chibon <pingou@pingoured.fr>
2012-01-18 16:58:19 +0100

    Clean up code
    - Use a logger
    - Replace print by a call to the logger
    - No need for a main function
    - Rename goutil to godistance
    - Create the __init__.py for the module
    - Move generic method to the head of the module

---
+++ src/__init__.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+This project is licensed under the New BSD License:
+
+Copyright (c) 2012, Pierre-Yves Chibon
+
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+* Redistributions of source code must retain the above copyright notice,
+this list of conditions and the following disclaimer.
+* Redistributions in binary form must reproduce the above copyright
+notice, this list of conditions and the following disclaimer in the
+documentation and/or other materials provided with the distribution.
+* Neither the name of the Wageningen University nor the names of its
+contributors may be used to endorse or promote products derived from
+this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ''AS IS'' AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE
+LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+THE POSSIBILITY OF SUCH DAMAGE.
+"""
+
+import logging
+
+logging.basicConfig()
+LOG = logging.getLogger('golib')
+
+def get_logger():
+    """ Return the logger. """
+    return LOG
+
+
+
+def write_down_ontology(ontodata, datafile):
+    """ Writes to disk the provided ontology.
+    :arg ontodata, the hash of hash containing all the ontology
+    :arg datafile, the name of the file to which write the ontology.
+    """
+    stream = open(datafile, 'w')
+    cnt = 0
+    for key in ontodata.keys():
+        stream.write('\n[Term] \n')
+        cnt = cnt + 1
+        info = ontodata[key]
+        for infokey in info.keys():
+            if isinstance(info[infokey], list):
+                for entry in info[infokey]:
+                    stream.write(infokey + ': ' + entry + '\n')
+            else:
+                stream.write(infokey + ': ' + info[infokey] + '\n')
+    stream.close()
+    print '%s terms written to the file %s' %(cnt, datafile)
--- src/godistance.py
+++ src/godistance.py
@@ -42,30 +42,16 @@ difference between the two terms.
 
 import datetime
 import os
+import sys
 import urllib
 
+try:
+    from pygolib import get_logger write_down_ontology
+except ImportError:
+    sys.path.insert(0, os.path.abspath('../'))
+    from src import get_logger write_down_ontology
+
 GOURL = 'http://geneontology.org/ontology/obo_format_1_2/gene_ontology_ext.obo'
-
-
-def write_down_ontology(ontodata, datafile):
-    """ Writes to disk the provided ontology.
-    :arg ontodata, the hash of hash containing all the ontology
-    :arg datafile, the name of the file to which write the ontology.
-    """
-    stream = open(datafile, 'w')
-    cnt = 0
-    for key in ontodata.keys():
-        stream.write('\n[Term] \n')
-        cnt = cnt + 1
-        info = ontodata[key]
-        for infokey in info.keys():
-            if isinstance(info[infokey], list):
-                for entry in info[infokey]:
-                    stream.write(infokey + ': ' + entry + '\n')
-            else:
-                stream.write(infokey + ': ' + info[infokey] + '\n')
-    stream.close()
-    print '%s terms written to the file %s' %(cnt, datafile)
 
 
 class GoDistanceCounterException(Exception):
@@ -85,11 +71,12 @@ class GoDistanceCounter(object):
         self.godata = None
         self.goterms = {}
         self.biological_process = {}
+        self.log = get_logger()
 
     def __do_handle_parent(self, termid, level, pred, paths, verbose=False):
         """ Handle the output for one parent of a term.
         It will retrieve the GO term for the given ID, add it to the pred
-        building up the path and keep building the upper part of the 
+        building up the path and keep building the upper part of the
         tree.
         If verbose is True, it will print the line of the tree.
 
@@ -140,8 +127,6 @@ class GoDistanceCounter(object):
         if path2 is None:
             path2 = self.get_path(self.goterms[goid2])
 
-        print goid1, path1
-        print goid2, path2
         ancester = None
         mindist = None
         deltalevel = None
@@ -159,7 +144,6 @@ class GoDistanceCounter(object):
                         mindist = dist
                         ancester = inter
                         deltalevel = deltaleveltmp
-        print ancester, deltalevel, mindist
         return mindist + deltalevel / 10.0
 
     def __score_parents(self, goid1, goid2, path1=None, path2=None):
@@ -172,8 +156,6 @@ class GoDistanceCounter(object):
         :kwarg path2, the list path from the second GO term to the top
         of the tree, as returned by get_path().
         """
-        #print goid1, path1
-        #print goid2, path2
         if path1 is None:
             path1 = self.get_path(self.goterms[goid1])
         scores = []
@@ -207,7 +189,6 @@ class GoDistanceCounter(object):
             pathways = self.get_path(term, pred=term['id'], paths=[],
                 verbose=verbose)
             for el in pathways:
-                #print "  ", el
                 if el.endswith('GO:0008150') and \
                         term['id'] not in self.biological_process.keys():
                     self.biological_process[term['id']] = term
@@ -227,7 +208,7 @@ class GoDistanceCounter(object):
             datetime.datetime.now().strftime('%Y%m%d')
         if not force_dl and not go_file \
                 and not os.path.exists(possible_go_file):
-            print "Retrieving GO from %s" % GOURL
+            self.log.info("Retrieving GO from %s" % GOURL)
             urllib.urlretrieve(GOURL, possible_go_file)
             go_file = possible_go_file
 
@@ -238,7 +219,7 @@ class GoDistanceCounter(object):
             raise GoDistanceCounterException(
                 'No file containing the GO ontology found or provided.')
 
-        print 'Reading file: %s' % go_file
+        self.log.info('Reading file: %s' % go_file)
         gostream = open(go_file)
         self.godata = gostream.read()
         gostream.close()
@@ -247,7 +228,7 @@ class GoDistanceCounter(object):
         """ From the GO annotation file, extract all the GO terms and
         store them into dictionnary.
         """
-        print 'Loading GO terms...'
+        self.log.info('Loading GO terms...')
         for entry in self.godata.split("\n\n"):
             if '[Term]' in entry:
                 info = {}
@@ -266,13 +247,12 @@ class GoDistanceCounter(object):
                 if info['id'] not in self.goterms.keys():
                     self.goterms[info['id']] = info
 
-        print "%s GO terms retrieved" % len(self.goterms.keys())
+        self.log.info("%s GO terms retrieved" % len(self.goterms.keys()))
 
     def get_path(self, term, level=0, pred="", paths=[], verbose=False):
         """ This is an iterative method which is used to retrieve the top
         parent of a given term.
         """
-        #print term['id'], paths, pred
         if 'is_a' in term.keys():
             if isinstance(term['is_a'], list):
                 before = pred
@@ -287,34 +267,6 @@ class GoDistanceCounter(object):
             paths.append(pred)
             return paths
 
-    def main(self, go_file=None):
-        """ Main method of the class.
-        Download the latest GO annotation and save it to the current
-        directory if no file was specified.
-        Reads in the GO annotation.
-        Builds the tree.
-        """
-        starttime = datetime.datetime.now()
-        self.get_go_data(go_file)
-        self.get_go_terms()
-        #self.get_biological_process()
-        #print "%s terms found in the biological process branch" % \
-            #len(self.biological_process.keys())
-        #outputfile =  'geneontology_biological_process-%s.obo' % \
-            #datetime.datetime.now().strftime('%Y%m%d')
-        #write_down_ontology(self.biological_process, outputfile)
-
-        #term = self.goterms['11']
-        #term = self.goterms['4']
-        #print term['id']
-        #self.get_path(term, pred=term['id'], paths=[], verbose=True)
-
-        self.scores('11', '0')
-
-        endtime  = datetime.datetime.now()
-        print "Time spent: ", endtime - starttime, "minutes"
-        self.godata=None
-
     def scores(self, id1, id2):
         """Returns the score between two given GO terms.
         :arg id1, identifier of a GO term (ie: GO:XXX).
@@ -326,17 +278,38 @@ class GoDistanceCounter(object):
         path2 = self.get_path(goterm2, pred=goterm2['id'], paths=[])
         scores = self.__score_parents(id1, id2, path1, path2)
         if scores:
-            print "%s and %s are parents" % (id1, id2)
-            print "The score between %s and %s is: %s" % (id1, id2, min(scores))
+            scores = min(scores)
+            self.log.info("%s and %s are parents" % (id1, id2))
         else:
             scores = self.__score_cousins(id1, id2, path1, path2)
-            if scores:
-                print "The score between %s and %s is: %s" % (id1, id2, scores)
-        
+        self.log.info("The score between %s and %s is: %s" % (id1, id2,
+            scores))
+        return scores
+
 
 if __name__ == '__main__':
     gdc = GoDistanceCounter()
-    #gdc.main()
-    #gdc.main(go_file='geneontology-20120117.all.obo')
-    gdc.main('test.obo')
-    #gdc.main('multi_is_a.obo')
+    #go_file=None
+    #go_file='geneontology-20120117.all.obo'
+    go_file = '../tests/test.obo'
+    
+    starttime = datetime.datetime.now()
+    gdc.get_go_data(go_file)
+    gdc.get_go_terms()
+    gdc.data = None
+    #gdc.get_biological_process()
+    #print "%s terms found in the biological process branch" % \
+        #len(self.biological_process.keys())
+    #outputfile =  'geneontology_biological_process-%s.obo' % \
+        #datetime.datetime.now().strftime('%Y%m%d')
+    #write_down_ontology(self.biological_process, outputfile)
+
+    #term = self.goterms['11']
+    #term = self.goterms['4']
+    #print term['id']
+    #gdc.get_path(term, pred=term['id'], paths=[], verbose=True)
+
+    gdc.scores('11', '0')
+
+    endtime  = datetime.datetime.now()
+    print "Time spent: ", endtime - starttime, "minutes"
