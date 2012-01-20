876d708eb673cecb9b0a678c3d72901cbef733a5
Pierre-Yves Chibon <pingou@pingoured.fr>
2012-01-20 13:19:16 +0100

    pylint/pep8 clean up

--- setup.py
+++ setup.py
@@ -4,17 +4,17 @@ Setup script
 """
 
 from distutils.core import setup
-from src import __version__ 
+from src import __version__
 
 setup(
-    name = 'pygolib',
-    description = 'Tools to manipulate GO term and graphs.',
-    author = 'Pierre-Yves Chibon',
-    author_email = 'pingou@pingoured.fr',
-    version = __version__,
-    license = 'BSD',
-    url = 'https://github.com/PBR/pygolib/',
-    package_dir = {'pygolib': 'src'},
-    packages = ['pygolib'],
+    name='pygolib',
+    description='Tools to manipulate GO term and graphs.',
+    author='Pierre-Yves Chibon',
+    author_email='pingou@pingoured.fr',
+    version=__version__,
+    license='BSD',
+    url='https://github.com/PBR/pygolib/',
+    package_dir={'pygolib': 'src'},
+    packages=['pygolib'],
     #scripts=["src/pygoutil"],
     )
